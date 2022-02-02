14db3bcb7497cd12820f5e7ea3808f279695c683
Jim Gray <jgray8689@gmail.com>
2022-02-01 21:50:35 -0500

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,22 +11,30 @@ export default function Footer() {
     return (
         <footer>
             <div>
-                <img
-                    src={twIcon}
-                    alt="Twitter"
-                />
-                <img
-                    src={fbIcon}
-                    alt="Facebook"
-                />
-                <img
-                    src={inIcon}
-                    alt="Instagram"
-                />
-                <img
-                    src={ghIcon}
-                    alt="GitHub"
-                />
+                <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
+                    <img
+                        src={twIcon}
+                        alt="Twitter"
+                    />
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                    <img
+                        src={fbIcon}
+                        alt="Facebook"
+                    />
+                </a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                    <img
+                        src={inIcon}
+                        alt="Instagram"
+                    />
+                </a>
+                <a href="https://github.com/JimGray9999" target="_blank" rel="noreferrer">
+                    <img
+                        src={ghIcon}
+                        alt="GitHub"
+                    />
+                </a>
             </div>
         </footer>
     )
