a47a78393b375186d451a7f9eaf1e2ccea4f5131
Jim Gray <jgray8689@gmail.com>
2022-02-01 21:41:29 -0500

    finishing up image and CSS sizing

--- src/components/TopImage.js
+++ src/components/TopImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 // exports the component holding the portfolio image
 export default function TopImage() {
     return (
-        <div className="image-profile">
-        </div>
+        <header className="image-profile">
+        </header>
     )
 }
